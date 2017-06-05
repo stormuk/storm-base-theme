a6281cdeb70592f085bf892ccd2a3a6c46051147
Liam Gladdy <liam@gladdy.co.uk>
2017-06-05 13:04:33 +0100

    Add foundation webpack

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+import $ from 'jquery'
+import whatInput from 'what-input';
+
+window.$ = $;
+
+import './lib/foundation-explicit-pieces';
+
 $(document).ready(function() {
 	$(document).foundation();
 });
