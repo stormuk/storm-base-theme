0ea1fee819798575a0ff14f9c218f04f76a7e567
Liam Gladdy <liam@gladdy.co.uk>
2017-11-29 10:30:16 +0000

    Add jQuery back to foundation files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
+import $ from 'jquery';
 import whatInput from 'what-input';
+
+window.$ = $;
 
 import './lib/foundation-explicit-pieces';
 
 $(document).ready(function() {
+	
 	$(document).foundation();
+	
 });
